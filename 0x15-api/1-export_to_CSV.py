2da8e35afb27c6aa57d6b3addad0d02a8042f4ae
root <lokaehab2003@gmail.com>
2024-02-19 17:21:48 -0800

    fix csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,49 +1,18 @@
 #!/usr/bin/python3
-""" convert data to csv file to store"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
-
-if __name__ == '__main__':
-    """run the script as main file"""
-    # get the id as a param
-    if len(sys.argv) != 2:
-        print("usage: app user_id")
-        exit(1)
-
-    # get the id and the urls
+if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com"
-    user_data = f"{url}/users/{user_id}"
-    todo_data = f"{url}/todos?userId={user_id}"
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # request data
-    req_user_data = requests.get(user_data)
-    req_todo_data = requests.get(todo_data)
-
-    # convert requested date to dict
-    dict_user_data = req_user_data.json()
-    dict_todo_data = req_todo_data.json()
-
-    # get neded data from dicts
-    name = dict_user_data.get("name")
-    tasks = len(dict_todo_data)
-
-    # list the output as csv
-    list_data = []
-    for i in range(len(dict_todo_data)):
-        row_data = [
-            f'{dict_todo_data[i].get("userId")}',
-            f'{name}',
-            f'{dict_todo_data[i].get("completed")}',
-            f'{dict_todo_data[i].get("title")}'
-        ]
-        list_data.append(row_data)
-
-    # write data in csv file
-    file_name = f"{user_id}.csv"
-
-    with open(file_name, "w", newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerows(list_data[0:])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
