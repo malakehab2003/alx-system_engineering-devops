d94ed6d47995901827119603da5d0399fa2d1d6c
malakehab2003 <lokaehab2003@gmail.com>
2024-03-06 00:13:34 +0200

    advanced task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,4 @@ import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"][:10]
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print('None')
+    pass
