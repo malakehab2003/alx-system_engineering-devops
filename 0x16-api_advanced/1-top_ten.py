173ce51a60eba2970dcee2d11804fa3570f6e14b
malakehab2003 <lokaehab2003@gmail.com>
2024-03-05 23:12:27 +0200

    get title of subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-"""get top 10 posts"""
-import requests
+# #!/usr/bin/python3
+# """get top 10 posts"""
+# import requests
 
 
-def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"][:10]
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print('None')
+# def top_ten(subreddit):
+#     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+#     headers = {"User-Agent": "Mozilla/5.0"}
+#     response = requests.get(url, headers=headers, allow_redirects=False)
+#     if response.status_code == 200:
+#         data = response.json()
+#         posts = data["data"]["children"][:10]
+#         for post in posts:
+#             print(post['data']['title'])
+#     else:
+#         print('None')
