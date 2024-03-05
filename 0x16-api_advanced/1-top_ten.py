c2b9463ff6900eb8314c511df2f3159530c29ce7
malakehab2003 <lokaehab2003@gmail.com>
2024-03-05 23:01:25 +0200

    get title of subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {"User-Agent":  'Chrome/111.0.0.0 Safari/537.36', allow_redirects=False}
+    headers = {"User-Agent":  'Chrome/111.0.0.0 Safari/537.36'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
