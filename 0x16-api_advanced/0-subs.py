c6b144316ac0e12fe78321f1924bd7c4d1799690
malakehab2003 <lokaehab2003@gmail.com>
2024-03-06 00:15:34 +0200

    advanced task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,4 +4,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    pass
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        no_of_subs = data['data']['subscribers']
+        return (no_of_subs)
+    else:
+        return (0)
